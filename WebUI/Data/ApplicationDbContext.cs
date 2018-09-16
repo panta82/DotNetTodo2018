2f1827ca2d5dcc0c498ccf2ade8ee52c2c0531f5
Ivan Pantic <ivanpantic82@gmail.com>
2018-09-16 19:45:30 +0200

    Stuck on broken shit

--- WebUI/Data/ApplicationDbContext.cs
+++ WebUI/Data/ApplicationDbContext.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebUI.Models;
 
 namespace WebUI.Data {
 	public class ApplicationDbContext : IdentityDbContext {
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options) {
 		}
+
+		public DbSet<TodoItem> Items { get; set; }
 	}
 }
